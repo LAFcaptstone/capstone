54889887de4fa339353f69b62b2400cb81234ecd
Corey Kepple <coreyckepple@gmail.com>
2014-04-07 15:39:04 -0500

    Created database migrations for lost_items

--- app/config/database.php
+++ app/config/database.php
@@ -55,9 +55,9 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'database',
-			'username'  => 'root',
-			'password'  => '',
+			'database'  => 'capstone_db',
+			'username'  => 'corey',
+			'password'  => 'password',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
