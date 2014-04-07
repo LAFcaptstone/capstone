be9457d9a52984aeba83305a0c863393714744af
Starnes81 <starnes81@gmail.com>
2014-04-07 15:28:42 -0500

    Layouts
    Created master layout and home layout. Created route and update
    HomeController to reflect route.

---
+++ app/views/home.blade.php
@@ -0,0 +1,100 @@
+@extends('layouts.master')
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="shortcut icon" href="../../assets/ico/favicon.ico">
+
+    <title>VIND IT!</title>
+
+   
+
+    <!-- Custom styles for this template -->
+    <!-- Latest compiled and minified CSS -->
+    <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    
+    <link href="/bootstrap/css/signin.css" rel="stylesheet">
+    <style type="text/css">
+
+    body {
+        margin-left: 0px;
+        width: 1250px;
+        /*background-image:url("img/maps.png");*/
+        
+        
+    }
+    .container {
+        margin: auto;
+        margin-top: 200px;
+        width: 100px;
+    }
+
+    .bttn1 {
+        height: 150px;
+        width: 150px;
+        background-color:  #3d983d;
+        margin-bottom:  10px;
+        color: #FFF;
+        font-size: 28px;
+        box-shadow: 0px 0px 20px #a3a3a3;
+    }
+    .bttn2 {
+        height: 150px;
+        width: 150px;
+        background-color: #c93131;
+        margin-bottom:  10px;
+        border: 2px solid: black;
+        color: #FFF;
+        font-size: 28px;
+        box-shadow: 0px 0px 20px #a3a3a3;
+    }
+
+    .bttn3 {
+        height: 40px;
+        width: 150px;
+        background-color: #FFF;
+        box-shadow: 0px 0px 20px #a3a3a3;
+    }
+    .text1 {
+        padding-top: 60px;
+        text-align: center;
+    }
+    .text2 {
+        
+        padding-top: 2px;
+        font-size: 28px;
+        text-align: center;
+    }
+
+
+    
+
+    </style>
+<!-- Latest compiled and minified JavaScript -->
+  </head>
+
+  <body src="img/maps">
+
+    <div class="container">
+        <a href="#"><div class="bttn1">
+            <p class="text1">FOUND IT!</p>
+        </div></a>
+        <a href="#"><div class="bttn2">
+             <p class="text1">LOST IT!</p>
+        </div></a>
+        <a href="#"><div class="bttn3">
+             <p class="text2">BROWSE!</p>
+        </div></a>
+    </div> <!-- /container -->
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+  </body>
+<script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
+</html>
---
+++ app/views/layouts/master.blade.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html>
+<head>
+	<title>VINED IT</title>
+</head>
+<body>
+	<!-- yeilding content from blades -->
+@yield('content')
+</body>
+<!-- contact email -->
+<footer>
+	<a href="mailto:support@findit.us?Subject=Hello%20again" target="_top"><p>CONTACT US</p></a>
+</footer>
+</html>
