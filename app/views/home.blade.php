c8501d9ccc06bb5dc720a26959f5dc60c10dc336
Starnes81 <starnes81@gmail.com>
2014-04-08 09:18:51 -0500

    updated class
    updated classes

---
+++ app/views/home.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts.master')
+
+
+    <div class="container centered-block">
+        <div class="row">
+            <a href="#"><div class=" col-md-6 col-md-offset-3 bttn1">
+                <p class="text1">FOUND IT!</p>
+            </div></a>
+        </div>
+        <div class="row">
+            <a href="#"><div class="col-md-6 col-md-offset-3 bttn2">
+                 <p class="text1">LOST IT!</p>
+            </div></a>
+        </div>
+        <div class="row">
+            <a href="#"><div class="col-md-6 col-md-offset-3 bttn3">
+                 <p class="text2">BROWSE!</p>
+            </div></a>
+        </div>
+    </div> <!-- /container -->
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+  </body>
+<script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
+</html>
