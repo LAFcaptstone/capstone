e35ec520d934c78236eaf338f6973164c7973f2f
Corey Kepple <coreyckepple@gmail.com>
2014-04-08 09:46:36 -0500

    added rules to model and changed input type ofbody

--- app/models/FoundItem.php
+++ app/models/FoundItem.php
@@ -3,5 +3,13 @@
 class FoundItem extends Base {
 
     protected $table = 'found_items';
+      /**
+      * Validation Rules
+      */ 
+    public static $rules = array(
+    'title'      => 'required|max:100',
+    'body'       => 'required|max:2000'
+	
+	);
 
 }
--- app/models/LostItem.php
+++ app/models/LostItem.php
@@ -4,4 +4,13 @@ class LostItem extends Base {
 
     protected $table = 'lost_items';
 
+     /**
+    * Validation Rules
+    */ 
+    public static $rules = array(
+    'title'      => 'required|max:100',
+    'body'       => 'required|max:2000'
+	
+	);
+
 }
