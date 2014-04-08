794c64e1ba4b9555a9b42657792035e5bb9137b4
Corey Kepple <coreyckepple@gmail.com>
2014-04-08 14:01:55 -0500

    made changes to found item rules

--- app/models/FoundItem.php
+++ app/models/FoundItem.php
@@ -8,7 +8,9 @@ class FoundItem extends Base {
       */ 
     public static $rules = array(
     'title'      => 'required|max:100',
-    'body'       => 'required|max:2000'
+    'body'       => 'required|max:2000',
+    'email'		 => 'required|max:100',
+    'location'   => 'required|max:10'
 	
 	);
 
