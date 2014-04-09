789cfdfb8782b034dbe492b2f9f735a77c6c1d3d
ceciliamunson <ceci_ugarte@hotmail.com>
2014-04-09 14:42:41 -0500

    Added showDashboard method

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -22,5 +22,13 @@ class HomeController extends BaseController {
 		return View::make('home')->with('data', $data);
 	}
 
+	// admin dashboard route
+	public function showDashboard()
+	{
+		$foundItems = FoundItem::all();
+		return View::make('dashboard')->with(array('foundItems' => $foundItems));
+	}
+
+
 
 }
