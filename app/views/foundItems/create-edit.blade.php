6a2822e05137965a194bd169a7cf7d3b73e58d96
ceciliamunson <ceci_ugarte@hotmail.com>
2014-04-09 14:43:38 -0500

    Added classes for styling

--- app/views/foundItems/create-edit.blade.php
+++ app/views/foundItems/create-edit.blade.php
@@ -1,17 +1,25 @@
 @extends('layouts.master')
 
 @section('content')
-<div class="container">
+
 @if (empty($foundItem->id))
 
-	<h1 style="margin-left:120px;">Post Found Item</h1>
+<div class="row">
+    <div class="col-sm-12">
+		<h1 style="margin-left:120px;">Post Found Item</h1>
+	</div>
+</div>
 
 	{{ Form::open(array('action' => 'FoundItemsController@store', 'files' => true, 'class' => 'form-horizontal')) }}
 		
 @else
 
-	<h1 class="blog-title">Edit Post</h1>
-	<hr>
+<div class="row">
+    <div class="col-sm-12">
+		<h1 class="blog-title">Edit Post</h1>
+	</div>
+</div>
+
 	{{ Form::model($foundItem, array('action' => array('FoundItemsController@update', $foundItem->id), 'method' => 'PUT', 'files' => true, 'class' => 'form-horizontal')) }}
 
 @endif
@@ -21,12 +29,16 @@
 				{{ Form::email('email', null, array('class' => 'form-control', 'placeholder' => 'Email')) }}
 		    	{{ $errors->has('email') ? $errors->first('email', '<p><span class="help-block">:message</span></p>') : " " }}
 		    </div>
+		</div>
+
+		<div class="form-group">
 		    {{ Form::label('location', 'Location', array('class' => 'col-sm-2 control-label')) }}
 		    <div class="col-sm-10">
 				{{ Form::text('location', null, array('class' => 'form-control', 'placeholder' => 'Zip Code')) }}
 		    	{{ $errors->has('location') ? $errors->first('location', '<p><span class="help-block">:message</span></p>') : " " }}
 		    </div>
 		</div>
+
 		<div class="form-group">
 		  	{{ Form::label('title', 'Title', array('class' => 'col-sm-2 control-label')) }}
 		  	<div class="col-sm-10">		
@@ -34,12 +46,14 @@
 		  		{{ $errors->has('title') ? $errors->first('title', '<p><span class="help-block">:message</span></p>') : " " }}
 		  	</div>
 		</div>
+
 		<div class="form-group">
 		  	{{ Form::label('body', 'Body', array('class' => 'col-sm-2 control-label')) }}
 		  	<div class="col-sm-10">		{{ Form::textarea('body', null, array('class' => 'form-control', 'row' => '5')) }}
 		    	{{ $errors->has('body') ? $errors->first('body', '<p><span class="help-block">:message</span></p>') : " " }}
 		  	</div>
 		</div>
+		
 		<div class="form-group">
 		    <div class="col-sm-offset-2 col-sm-10">
     			{{ Form::file('image') }}
@@ -48,7 +62,7 @@
 		      	<a href="{{{action('FoundItemsController@index') }}}">Cancel</a>
 		    </div>
 		</div>
+
 	{{ Form::close() }}
-</div>
 	
 @stop
