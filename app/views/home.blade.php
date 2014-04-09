daec54034b2573b80c3115644f502f32ed1322c5
Corey Kepple <coreyckepple@gmail.com>
2014-04-09 09:24:24 -0500

    made changes to master page and homepage

--- app/views/home.blade.php
+++ app/views/home.blade.php
@@ -1,33 +1,39 @@
 @extends('layouts.master')
 
 @section('content')
-    
-        <div class="row ">
-            <a href="{{{ action('FoundItemsController@create')}}}"><div class=" col-md-6 col-md-offset-5 bttn1">
-                <p class="text1">FOUND IT!</p>
-            </div></a>
-        </div>
-        <div class="row">
-            <a href="{{{ action('LostItemsController@create')}}}"><div class="col-md-6 col-md-offset-5 bttn2">
-                 <p class="text1">LOST IT!</p>
-            </div></a>
-        </div>
-        <div class="row">
-            <a href="#"><div class="col-md-6 col-md-offset-5 bttn3">
-                 <p class="text2">BROWSE!</p>
-            </div></a>
-        </div>
-   
-
-
+	{{{var_dump($_POST)}}}
+		<div class="row ">
+			<a href="{{{ action('FoundItemsController@create')}}}"><div class=" col-md-6 col-md-offset-5 bttn1">
+				<p class="text1">FOUND IT!</p>
+			</div></a>
+		</div>
+		<div class="row">
+			<a href="{{{ action('LostItemsController@create')}}}"><div class="col-md-6 col-md-offset-5 bttn2">
+				 <p class="text1">LOST IT!</p>
+			</div></a>
+		</div>
+		<div class="row">
+			<a href="#" id='lookup'><div class="col-md-6 col-md-offset-5 bttn3">
+				 <p class="text2">BROWSE!</p>
+			</div></a>
+		</div>
+		<div class="row" id='browse' name='browse' style='display:none;'>
+			<div class="col-md-6 col-md-offset-5 bttn4">
+				{{ Form::open(array('url' => '')) }}
+				{{ Form::select('dataset', array('F' => 'Found', 'L' => 'Lost'), 'F', array('class' => 'form-control')) }}
+				{{ Form::submit('Click Me!', array('class' => 'form-control')) }}
+				{{ Form::close() }}
+			</div>
+		</div>
   </body>
 
 @stop
-@section('bottomscript')
 
-    <!-- Bootstrap core JavaScript
-    ================================================== -->
-    <!-- Placed at the end of the document so the pages load faster -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
-    <script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
+@section('bottomscript')
+  <script>
+  $('#lookup').click(function (e){
+	e.preventDefault();
+	$('#browse').show();
+  });
+  </script>
 @stop
