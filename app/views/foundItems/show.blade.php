483c5ad1ee7410b84eb6a084195637c030248ba9
ceciliamunson <ceci_ugarte@hotmail.com>
2014-04-09 11:30:04 -0500

    Added edit and delete links and js script to delete post

--- app/views/foundItems/show.blade.php
+++ app/views/foundItems/show.blade.php
@@ -10,8 +10,26 @@
 
 <hr>
 <p><a href="mailto:support@findit.us?Subject=Hello%20again" target="_top" id="btnNotify">Notify</a></p>		
-<p><a href="{{{action('FoundItemsController@index') }}}">Return to List</a></p>
-    
+<p><a href="{{{action('FoundItemsController@index') }}}">Return to Found Items</a></p>
+<a href="{{{ action('FoundItemsController@edit', $foundItem->id) }}}">Edit Post</a> |
+<a href="{{{ action('FoundItemsController@destroy', $foundItem->id) }}}" id="btnDeletePost">Delete Post</a>
+{{ Form::open(array('action' => array('FoundItemsController@destroy', $foundItem->id), 'method' => 'delete', 'id' => 'deleteFormPost')) }}
+{{ Form::close() }}
 
 @stop
+
+@section('bottomscript')	
+	<script>
+
+	$('#btnDeletePost').on('click', function (e) {
+		e.preventDefault();
+		if (confirm('Are you sure you want to delete this post?')) {
+			$('#deleteFormPost').submit();
+		}
+	});
+
+	</script>
+
+@stop
+
 
--- app/views/lostItems/show.blade.php
+++ app/views/lostItems/show.blade.php
@@ -10,9 +10,24 @@
 
 <hr>
 <p><a href="mailto:support@findit.us?Subject=Hello%20again" target="_top" id="btnNotify">Notify</a></p>		
-<p><a href="{{{action('LostItemsController@index') }}}">Return to List</a></p>
-    
-
+<p><a href="{{{action('LostItemsController@index') }}}">Return to Lost Items</a></p>
+<a href="{{{ action('LostItemsController@edit', $lostItem->id) }}}">Edit Post</a> |
+<a href="{{{ action('LostItemsController@destroy', $lostItem->id) }}}" id="btnDeletePost">Delete Post</a>
+{{ Form::open(array('action' => array('LostItemsController@destroy', $lostItem->id), 'method' => 'delete', 'id' => 'deleteFormPost')) }}
+{{ Form::close() }}
 
 @stop
 
+@section('bottomscript')	
+	<script>
+
+	$('#btnDeletePost').on('click', function (e) {
+		e.preventDefault();
+		if (confirm('Are you sure you want to delete this post?')) {
+			$('#deleteFormPost').submit();
+		}
+	});
+
+	</script>
+
+@stop
