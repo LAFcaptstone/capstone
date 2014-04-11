1024e00d6e81b8d8edbbbb86b5a82dfc397738e5
Corey Kepple <coreyckepple@gmail.com>
2014-04-11 13:02:08 -0500

    added blue graduate to login page

--- app/views/login.blade.php
+++ app/views/login.blade.php
@@ -1,13 +1,13 @@
 @extends('layouts.master')
 
 @section('content')
-
+<div class='container-full'>
 <div class="container">    
     <div id="loginbox" style="margin-top:50px;" class="mainbox col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2">                    
         <div class="panel panel-info" >
             <div class="panel-heading">
                 <div class="panel-title">Sign In</div>
-                <div style="float:right; font-size: 80%; position: relative; top:-10px"><a href="#">Forgot password?</a></div>
+                <div style="float:right; font-size: 80%; position: relative; top:-10px"><a href="">Forgot password?</a></div>
             </div>     
 
         <div style="padding-top:30px" class="panel-body" >
@@ -40,7 +40,7 @@
                         <div class="col-md-12 control">
                             <div style="border-top: 1px solid#888; padding-top:15px; font-size:85%" >
                                 Don't have an account! 
-                                <a href="#" onClick="$('#loginbox').hide(); $('#signupbox').show()">
+                                <a href="#">
                                     Sign Up Here
                                 </a>
                             </div>
@@ -50,6 +50,7 @@
         	</div>                     
         </div>  
     </div>
+</div>
 </div>
 
 @stop
