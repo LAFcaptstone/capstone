2d850e34f77fa9d8c986a3868881859875dd88f8
ceciliamunson <ceci_ugarte@hotmail.com>
2014-04-11 13:05:11 -0500

    Added routes for flag method  in Found and Lost items controller

--- app/routes.php
+++ app/routes.php
@@ -21,11 +21,17 @@ Route::post('/login', 'HomeController@doLogin');
 
 Route::get('/logout', 'HomeController@logout');
 
+Route::post('/foundItems/{id}/flag', 'FoundItemsController@flag');
+
+Route::post('/lostItems/{id}/flag', 'LostItemsController@flag');
+
 Route::resource('foundItems', 'FoundItemsController');
 
 Route::resource('lostItems', 'LostItemsController');
 
 Route::get('/map', 'HomeController@showMap');
+
+Route::controller('password', 'RemindersController');
 
 Route::get('/test', function(){
 	return View::make('newhome');
