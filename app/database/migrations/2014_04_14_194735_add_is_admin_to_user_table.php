e90ad154f2e6c415e5532c873685eb5911512048
Starnes81 <starnes81@gmail.com>
2014-04-14 14:57:04 -0500

    is_admin migration
    is admin added to user table

--- app/database/migrations/2014_04_14_194735_add_is_admin_to_user_table.php
+++ app/database/migrations/2014_04_14_194735_add_is_admin_to_user_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreatePasswordRemindersTable extends Migration {
+class AddIsAdminToUserTable extends Migration {
 
 	/**
 	 * Run the migrations.
@@ -12,11 +12,9 @@ class CreatePasswordRemindersTable extends Migration {
 	 */
 	public function up()
 	{
-		Schema::create('password_reminders', function(Blueprint $table)
+		Schema::table('users', function($table)
 		{
-			$table->string('email')->index();
-			$table->string('token')->index();
-			$table->timestamp('created_at');
+    		$table->tinyInteger('is_admin');
 		});
 	}
 
@@ -27,7 +25,11 @@ class CreatePasswordRemindersTable extends Migration {
 	 */
 	public function down()
 	{
-		Schema::drop('password_reminders');
+		Schema::table('users', function($table) {
+
+			$table->dropColumn('is_admin');
+
+		});
 	}
 
 }
