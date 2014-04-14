20ee3ea7319ab46c25cf83c1ac3fe17d98e8ecbe
Starnes81 <starnes81@gmail.com>
2014-04-14 14:57:45 -0500

    foreignkey migration
    Migration for foreign key added

---
+++ app/database/migrations/2014_04_14_162452_add_forignkey_to_user_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddForignkeyToUserTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::table('found_items', function($table)
+		{
+		    $table->integer('user_id')->unsigned()->nullable();
+      	    $table->foreign('user_id')->references('id')->on('users');		    
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::table('found_items', function($table) 
+		{
+			$table->dropForeign('found_items_user_id_foreign');
+			$table->dropColumn('user_id');
+		});
+	}
+}
