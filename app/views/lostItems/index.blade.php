d92e43c9e7bfb918799b9efa9be5d82d78173a26
ceciliamunson <ceci_ugarte@hotmail.com>
2014-04-15 09:56:10 -0500

    Changed style/layout of index view

--- app/views/lostItems/index.blade.php
+++ app/views/lostItems/index.blade.php
@@ -1,10 +1,5 @@
 @extends('layouts.master')
 
-@section('topscript')
-<link href='css/listview.css' rel='stylesheet'>
-@stop
-
-
 @section('content')
 
 <!-- NavBar -->
@@ -20,7 +15,7 @@
       <a href="/" class="navbar-brand">Vind iT</a>
     </div>
     <nav class="collapse navbar-collapse" role="navigation">
-    {{ Form::open(array('action' => array('FoundItemsController@index'), 'method' => 'GET', 'class' => 'navbar-form navbar-left')) }}
+    {{ Form::open(array('action' => array('LostItemsController@index'), 'method' => 'GET', 'class' => 'navbar-form navbar-left')) }}
     <!-- <div class="form-group"> -->
         <!-- <input type="text" class="form-control" placeholder="Search"> -->
     <!-- </div> -->
@@ -29,8 +24,8 @@
         {{ Form::submit('Search', array('class'=> 'btn btn-default')) }}
     {{ Form::close() }} 
       <ul class="nav navbar-right navbar-nav">
-        <li>
-          <a href="{{{ action('FoundItemsController@create') }}}">Create New Post</a>
+         <li>
+          <a href="{{{ action('LostItemsController@create') }}}">Create New Post</a>
         </li>
         <li>
             @if(Auth::check())
@@ -45,61 +40,35 @@
         <li>
           <a href="#">Contact Us</a>
         </li>
-        
       </ul>
     </nav>
   </div>
 </header>
 <!-- NavBar -->
 
-
-<div class="container">
-  <div class="row">
-    
-    <div class="col-md-12"> 
-      
-      <div class="panel">
-        <div class="panel-body">
-          
-          
-          <h1 style='margin-bottom:75px; margin-top:30px;'>Whats been lost...</h1>
-          <!--/stories-->
-          @foreach ($lostItems as $lostItem)
-          <div class="row">    
-            <br>
-            <div class="col-md-2 col-sm-3 text-center">
-            @if (!is_null($lostItem->image_path)) 
-              <a class="story-title" href="{{{ action('LostItemsController@show', $lostItem->id) }}}"><img alt="Post specific image" src="{{ $lostItem->image_path }}" style="width:100px;height:100px" class="img-circle"></a>
+<div class="portfolio container">
+    <div class="portfolio-title">
+        <h3>Whats been lost...</h3>
+    </div>
+    <div class="row">
+    @foreach ($lostItems as $lostItem)
+        <div class="col-md-3">
+        	@if (!is_null($lostItem->image_path))
+            	<a href="{{{ action('LostItemsController@show', $lostItem->id) }}}"><img src="{{ $lostItem->image_path }}" alt="" style="width:200px;height:150px"></a>
+            @else
+            	<a href="{{{ action('LostItemsController@show', $lostItem->id) }}}"><img src="/img/vind.jpeg" alt="" style="width:200px;height:150px"></a>
             @endif
+            <a href="{{{ action('LostItemsController@show', $lostItem->id) }}}"><h3>{{{ $lostItem->title }}}</h3></a>
+            <div class="icon-awesome">
+                <h4><small style="font-family:courier,'new courier';" class="text-muted">{{{ $lostItem->created_at->format('l, F jS Y @ h:i:s A') }}}</small></h4>
+                <h4><span class="label label-default">Location: {{{ $lostItem->location }}}</span></h4>
             </div>
-            <div class="col-md-10 col-sm-9">
-              <a href="{{{ action('LostItemsController@show', $lostItem->id) }}}"><h3>{{{ $lostItem->title }}}</h3></a>
-              <div class="row">
-                <div class="col-xs-9">
-                  <h4><span class="label label-default">Location: {{{ $lostItem->location }}}</span></h4><h4>
-                  <small style="font-family:courier,'new courier';" class="text-muted">Posted at: {{{ $lostItem->created_at }}}</small>
-                  </h4></div>
-                <div class="col-xs-3"></div>
-              </div>
-              <br><br>
-            </div>
-          </div>
-          <hr>
-          <hr>
-          @endforeach
-          <!--/stories-->
-          
-          
-          {{ $lostItems->appends(array('search' => Input::get('search')))->links() }}
-        
-          
         </div>
-      </div>
-                                                                                       
-                                                    
-                                                      
-    </div><!--/col-12-->
-  </div>
+    @endforeach
+    </div>
+
+    {{ $lostItems->appends(array('search' => Input::get('search')))->links() }}
 </div>
 
 @stop
+
