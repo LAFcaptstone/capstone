fbd319e23e0ddf0ebb042125bf935a02b653ce91
ceciliamunson <ceci_ugarte@hotmail.com>
2014-04-17 10:38:30 -0500

    Testing with style

--- app/views/foundItems/index.blade.php
+++ app/views/foundItems/index.blade.php
@@ -8,22 +8,20 @@
 
 <div class="portfolio">
     <div class="portfolio-title">
-
-
-        <h3>Whats been found...</h3>
+        <h1>Whats been found...</h1>
             <p><a href="{{{action('FoundItemsController@index')}}}" class="btn btn-success btn-md">What's been found...<span class="glyphicon glyphicon-circle-arrow-right"></span></a><a href="{{{action('LostItemsController@index')}}}" class="btn btn-danger btn-md">What's been lost...<span class="glyphicon glyphicon-circle-arrow-right"></span></a></p>
-
-
     </div>
     <div class="row">
     @foreach ($foundItems as $foundItem)
         <div class="col-md-2 col-md-offset-1 work">
-        	@if (!is_null($foundItem->image_path))
-            	<a href="{{{ action('FoundItemsController@show', $foundItem->id) }}}"><img src="{{ $foundItem->image_path }}" alt="" class="img-responsive"></a>
-            @else
-            	<a href="{{{ action('FoundItemsController@show', $foundItem->id) }}}"><img src="/img/vind.jpeg" alt="" class="img-responsive"></a>
-            @endif
-            <a href="{{{ action('FoundItemsController@show', $foundItem->id) }}}"><h3>{{{ $foundItem->title }}}</h3></a>
+            <div style="width:125px;height:125px">
+        	   @if (!is_null($foundItem->image_path))
+                	<a href="{{{ action('FoundItemsController@show', $foundItem->id) }}}"><img src="{{ $foundItem->image_path }}" alt="" class="img-responsive"></a>
+                @else
+                	<a href="{{{ action('FoundItemsController@show', $foundItem->id) }}}"><img src="/img/vind.jpeg" alt="" class="img-responsive"></a>
+                @endif
+                <a href="{{{ action('FoundItemsController@show', $foundItem->id) }}}"><h3>{{{ $foundItem->title }}}</h3></a>
+            </div>
             <div class="icon-awesome">
                 <h4><small style="font-family:courier,'new courier';" class="text-muted">{{{ $foundItem->created_at->format('l, F jS Y @ h:i:s A') }}}</small></h4>
                 <h4><span class="label label-default">Location: {{{ $foundItem->location }}}</span></h4>
