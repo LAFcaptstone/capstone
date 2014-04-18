6f7af502095b3d63c326179dfb18668a630bfacb
Corey Kepple <coreyckepple@gmail.com>
2014-04-18 13:46:49 -0500

    changed master blade to work with new layout + css

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -12,56 +12,57 @@
 		<link rel="apple-touch-icon" sizes="72x72" href="/bootstrap/img/apple-touch-icon-72x72.png">
 		<link rel="apple-touch-icon" sizes="114x114" href="/bootstrap/img/apple-touch-icon-114x11png">
 		<link href="//netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.min.css" type="text/css" rel="stylesheet">
-		<link href='/css/base.css' rel='stylesheet'>
+		<link href='/css/master.css' rel='stylesheet'>
 		
 		@yield('topscript')
 	</head>	
-		<body>
-				<div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
-					<div class="navbar-header">
-						<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-						<span class="sr-only">Toggle navigation</span>
-						<span class="icon-bar"></span>
-						<span class="icon-bar"></span>
-						<span class="icon-bar"></span>
-						</button>
-						<a class="navbar-brand" rel="home" href="{{{action('HomeController@showWelcome')}}}">Vind iT</a>
-					</div>
-			
-			<div class="collapse navbar-collapse">
-				
-				<ul class="nav navbar-nav">
-					@if(Auth::check() && Auth::user()->is_admin == 1)
-					<li><a href="{{{ action('HomeController@showFoundItemsDashboard') }}}" >Welcome ({{{Auth::user()->first_name}}})</a></li>
-					@elseif(Auth::check() && Auth::user()->is_admin == 2)
-					<li><a href="{{{ action('UserController@show', Auth::user()->id) }}}" >Welcome ({{{Auth::user()->first_name}}})</a></li>
-					@else
-					<li><a href="{{{ action('HomeController@showLogin') }}}" >Login</a></li>
-					@endif
-					<li><a href="{{{ action('HomeController@showContact') }}}">Contact Us</a></li>
-					<li><a href="{{{ action('FoundItemsController@create') }}}" style='color:green;'>New Found Post</a></li>
-					<li><a href="{{{ action('LostItemsController@create') }}}" style='color:#F00;'>New Lost Post</a></li>
-				</ul>
-				
-				<form class="navbar-form">
-        			<div class="form-group" style="display:inline;">
-        			  <div class="input-group">
-        			    <div class="input-group-btn">
-        			      <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown">View Posts</button>
-        			      <ul class="dropdown-menu">
-        			        <li class="found-items"><a href="{{{ action('HomeController@searchFoundItems') }}}">Found Items</a></li>
-        			        <li class="lost-items"><a href="{{{ action('HomeController@searchLostItems') }}}">Lost Items</a></li>
-        			      </ul>
-        			    </div>
-        			    @yield('itemtype')
-        			    <input type="text" name="search" id="searching" class="form-control" placeholder="What are searching for?" {{ Request::is('searchFoundItems') || Request::is('searchLostItems') ? '' : "disabled" }}>
-        			    <span class="input-group-addon"><button type='submit'><span class="glyphicon glyphicon-search"></span></button></span>
-        			  </div>
-        			</div>
-      			</form>
-				
-			</div>
-		</div>
+	<body>
+		<nav class="navbar navbar-trans navbar-fixed-top" role="navigation">
+  			<div class="container">
+    
+			    <div class="navbar-header">
+			      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-collapsible">
+			        <span class="sr-only">Toggle navigation</span>
+			        <span class="icon-bar"></span>
+			        <span class="icon-bar"></span>
+			        <span class="icon-bar"></span>
+			      </button>
+			      <a class="navbar-brand" href="{{{action('HomeController@showWelcome')}}}">Vind iT</a>
+			    </div>
+    
+    			<div class="navbar-collapse collapse" id="navbar-collapsible">
+				    <ul class="nav navbar-nav navbar-left">
+					    @if(Auth::check() && Auth::user()->is_admin == 1)
+						<li><a href="{{{ action('HomeController@showFoundItemsDashboard') }}}" >Welcome ({{{Auth::user()->first_name}}})</a></li>
+						@elseif(Auth::check() && Auth::user()->is_admin == 2)
+						<li><a href="{{{ action('UserController@show', Auth::user()->id) }}}" >Welcome ({{{Auth::user()->first_name}}})</a></li>
+						@else
+						<li><a href="{{{ action('HomeController@showLogin') }}}" >Login</a></li>
+						@endif
+						<li><a href="{{{ action('HomeController@showContact') }}}">Contact Us</a></li>
+						<li><a href="{{{ action('FoundItemsController@create') }}}" style='color:green;'>New Found Post</a></li>
+						<li><a href="{{{ action('LostItemsController@create') }}}" style='color:#F00;'>New Lost Post</a></li>
+					    <li>&nbsp;</li>
+				    </ul>
+   
+			        <form class="navbar-form">
+			          <div class="form-group" style="display:inline;">
+			            <div class="input-group">
+			              <div class="input-group-btn">
+			                	<button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown">View Posts</button>
+			                	<ul class="dropdown-menu">
+			          			<li class="found-items"><a href="{{{ action('HomeController@searchFoundItems') }}}">Found Items</a></li>
+			          			<li class="lost-items"><a href="{{{ action('HomeController@searchLostItems') }}}">Lost Items</a></li>
+			          		</ul>
+			              </div>
+			              <input type="text" name="search" id="searching" class="form-control" placeholder="What are searching for?" {{ Request::is('searchFoundItems') || Request::is('searchLostItems') ? '' : "disabled" }}>
+			          	<span class="input-group-addon"><button type='submit'><span class="glyphicon glyphicon-search"></span></button></span>
+			            </div>
+			          </div>
+			        </form>
+    			</div>
+ 	 		</div>
+		</nav>
 		<div class="container">
 		@if (Session::has('successMessage'))
 			<div class="alert alert-success" style='margin-top:50px;'>{{{ Session::get('successMessage') }}}<button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button></div>
@@ -69,7 +70,7 @@
 		@if (Session::has('errorMessage'))
 			<div class="alert alert-danger" style='margin-top:50px;'>{{{ Session::get('errorMessage') }}}<button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button></div>
 		@endif
-	</div>
+		</div>
 	
 	   	<!-- yeilding content from blades -->
 		@yield('content')
