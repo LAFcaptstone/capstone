9b21802d4289d5f2496baa5d06b9d33eddfaeeb7
ceciliamunson <ceci_ugarte@hotmail.com>
2014-04-18 09:55:01 -0500

    messages&usersDashboard: added filter plugging to search through columns

--- app/views/usersDashboard.blade.php
+++ app/views/usersDashboard.blade.php
@@ -10,9 +10,9 @@
     <div class="row">
         <div class="col-sm-3 col-md-2 sidebar">
         	<ul class="nav nav-sidebar">
-        	 	<li class="active"><a href="{{{action('HomeController@showFoundItemsDashboard') }}}">Found Items</a></li>
+        	 	<li><a href="{{{action('HomeController@showFoundItemsDashboard') }}}">Found Items</a></li>
         	 	<li><a href="{{{action('HomeController@showLostItemsDashboard') }}}">Lost Items</a></li>
-        	 	<li><a href="{{{action('HomeController@showUsersDashboard') }}}">Users</a></li>
+        	 	<li class="active"><a href="{{{action('HomeController@showUsersDashboard') }}}">Users</a></li>
         		<li><a href="{{{action('HomeController@showMessagesDashboard') }}}">Messages</a></li>
         		<li><a href="{{{ action('HomeController@logout') }}}">Logout</a></li>
         	</ul>
@@ -21,41 +21,46 @@
 		<div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
         	<h1 class="page-header">Dashboard</h1>
 
-			<h3 class="sub-header">users</h3>
-			<table class="table table-responsive">
-				<thead>
-        		    <tr>
-        		       	<th>Id</th>
-        		       	<th>First Name</th>
-        		       	<th>Last Name</th>
-        		       	<th>Email</th>
-        		       	<th>Password</th>
-        		       	<th>Date Created</th>
-						<th>Date Updated</th>
-        		    </tr>
-        		</thead>
-  		
-				<tbody>
-				@foreach ($users as $user)
-       			    <tr>
-						<td>{{{ $user->id }}}</td>
-						<td>{{{ $user->first_name }}}</td>
-						<td>{{{ $user->last_name }}}</td>
-						<td>{{{ $user->email }}}</td>
-						<td>{{{ $user->password }}}</td>
-						<td>{{{ $user->created_at->format('l, F jS Y @ h:i:s A') }}}</td>
-						<td>{{{ $user->updated_at->format('l, F jS Y @ h:i:s A') }}}</td>
-						<!-- <td><a href="{{{ action('UserController@edit', $user->id) }}}">Edit</a> -->
-						<td>
-							{{ Form::open(array('action' => array('UserController@destroy', $user->id), 'method' => 'delete')) }}
-							{{ Form::submit('Delete', array('class' => 'btnDelete')) }}
-							{{ Form::close() }}
-						</td>
-					</tr>
-				@endforeach
-				</tbody>
-			</table>
-			{{ $users->links() }}
+			<div class="panel panel-primary filterable">
+            	<div class="panel-heading">
+            	    <h1 class="panel-title">Users</h1>
+            	    <div class="pull-right">
+            	        <button class="btn btn-default btn-xs btn-filter"><span class="glyphicon glyphicon-filter"></span> Filter</button>
+            	    </div>
+            	</div>
+				<table class="table table-responsive">
+					<thead>
+        			    <tr class="filters">
+        			       	<th><input type="text" class="form-control" placeholder="Id" disabled></th>
+        			       	<th><input type="text" class="form-control" placeholder="First Name" disabled></th>
+        			       	<th><input type="text" class="form-control" placeholder="Last Name" disabled></th>
+        			       	<th><input type="text" class="form-control" placeholder="Email" disabled></th>
+        			       	<th><input type="text" class="form-control" placeholder="Date Created" disabled></th>
+							<th><input type="text" class="form-control" placeholder="Date Updated" disabled></th>
+        			    </tr>
+        			</thead>
+  			
+					<tbody>
+					@foreach ($users as $user)
+       				    <tr>
+							<td>{{{ $user->id }}}</td>
+							<td>{{{ $user->first_name }}}</td>
+							<td>{{{ $user->last_name }}}</td>
+							<td>{{{ $user->email }}}</td>
+							<td>{{{ $user->created_at->format('l, F jS Y @ h:i:s A') }}}</td>
+							<td>{{{ $user->updated_at->format('l, F jS Y @ h:i:s A') }}}</td>
+							<!-- <td><a href="{{{ action('UserController@edit', $user->id) }}}">Edit</a> -->
+							<td>
+								{{ Form::open(array('action' => array('UserController@destroy', $user->id), 'method' => 'delete')) }}
+								{{ Form::submit('Delete', array('class' => 'btnDelete')) }}
+								{{ Form::close() }}
+							</td>
+						</tr>
+					@endforeach
+					</tbody>
+				</table>
+				{{ $users->links() }}
+			</div>
 		</div>
 	</div>
 </div>
@@ -73,6 +78,50 @@
 		if (confirm('Are you sure you want to delete this user?')) {
 			$(this).parent('form').submit();
 		}
+	});
+
+	// Filter pluggin
+	$(document).ready(function(){
+    	$('.filterable .btn-filter').click(function(){
+    	    var $panel = $(this).parents('.filterable'),
+    	    $filters = $panel.find('.filters input'),
+    	    $tbody = $panel.find('.table tbody');
+    	    if ($filters.prop('disabled') == true) {
+    	        $filters.prop('disabled', false);
+    	        $filters.first().focus();
+    	    } else {
+    	        $filters.val('').prop('disabled', true);
+    	        $tbody.find('.no-result').remove();
+    	        $tbody.find('tr').show();
+    	    }
+    	});
+	
+    	$('.filterable .filters input').keyup(function(e){
+    	    /* Ignore tab key */
+    	    var code = e.keyCode || e.which;
+    	    if (code == '9') return;
+    	    /* Useful DOM data and selectors */
+    	    var $input = $(this),
+    	    inputContent = $input.val().toLowerCase(),
+    	    $panel = $input.parents('.filterable'),
+    	    column = $panel.find('.filters th').index($input.parents('th')),
+    	    $table = $panel.find('.table'),
+    	    $rows = $table.find('tbody tr');
+    	    /* Dirtiest filter function ever ;) */
+    	    var $filteredRows = $rows.filter(function(){
+    	        var value = $(this).find('td').eq(column).text().toLowerCase();
+    	        return value.indexOf(inputContent) === -1;
+    	    });
+    	    /* Clean previous no-result if exist */
+    	    $table.find('tbody .no-result').remove();
+    	    /* Show all rows, hide filtered ones (never do that outside of a demo ! xD) */
+    	    $rows.show();
+    	    $filteredRows.hide();
+    	    /* Prepend no-result row if all rows are filtered */
+    	    if ($filteredRows.length === $rows.length) {
+    	        $table.find('tbody').prepend($('<tr class="no-result text-center"><td colspan="'+ $table.find('.filters th').length +'">No result found</td></tr>'));
+    	    }
+    	});
 	});
 
 </script>
