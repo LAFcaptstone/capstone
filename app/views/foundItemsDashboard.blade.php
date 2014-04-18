03f164244b8c7611d11e7297880de481e73831a4
ceciliamunson <ceci_ugarte@hotmail.com>
2014-04-18 09:54:30 -0500

    foundItemsDashboard: Deleted filter plugging
    It was messing the order by

--- app/views/foundItemsDashboard.blade.php
+++ app/views/foundItemsDashboard.blade.php
@@ -21,13 +21,7 @@
 		<div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
         	<h1 class="page-header">Dashboard</h1>
 		
-			<div class="panel panel-primary filterable">
-        	    <div class="panel-heading">
-        	        <h3 class="sub-header">Found Items Posts</h3>
-        	        <div class="pull-right">
-        	            <button class="btn btn-default btn-xs btn-filter"><span class="glyphicon glyphicon-filter"></span> Filter</button>
-        	        </div>
-        	    </div>
+        	    <h3 class="sub-header">Found Items Posts</h3>
 				<table class="table table-responsive">
 					<thead>
         			    <tr>
@@ -86,50 +80,6 @@
 		if (confirm('Are you sure you want to delete this post?')) {
 			$(this).parent('form').submit();
 		}
-	});
-
-	// Filter pluggin
-	$(document).ready(function(){
-    	$('.filterable .btn-filter').click(function(){
-    	    var $panel = $(this).parents('.filterable'),
-    	    $filters = $panel.find('.filters input'),
-    	    $tbody = $panel.find('.table tbody');
-    	    if ($filters.prop('disabled') == true) {
-    	        $filters.prop('disabled', false);
-    	        $filters.first().focus();
-    	    } else {
-    	        $filters.val('').prop('disabled', true);
-    	        $tbody.find('.no-result').remove();
-    	        $tbody.find('tr').show();
-    	    }
-    	});
-	
-    	$('.filterable .filters input').keyup(function(e){
-    	    /* Ignore tab key */
-    	    var code = e.keyCode || e.which;
-    	    if (code == '9') return;
-    	    /* Useful DOM data and selectors */
-    	    var $input = $(this),
-    	    inputContent = $input.val().toLowerCase(),
-    	    $panel = $input.parents('.filterable'),
-    	    column = $panel.find('.filters th').index($input.parents('th')),
-    	    $table = $panel.find('.table'),
-    	    $rows = $table.find('tbody tr');
-    	    /* Dirtiest filter function ever ;) */
-    	    var $filteredRows = $rows.filter(function(){
-    	        var value = $(this).find('td').eq(column).text().toLowerCase();
-    	        return value.indexOf(inputContent) === -1;
-    	    });
-    	    /* Clean previous no-result if exist */
-    	    $table.find('tbody .no-result').remove();
-    	    /* Show all rows, hide filtered ones (never do that outside of a demo ! xD) */
-    	    $rows.show();
-    	    $filteredRows.hide();
-    	    /* Prepend no-result row if all rows are filtered */
-    	    if ($filteredRows.length === $rows.length) {
-    	        $table.find('tbody').prepend($('<tr class="no-result text-center"><td colspan="'+ $table.find('.filters th').length +'">No result found</td></tr>'));
-    	    }
-    	});
 	});
 
 </script>
